fe57ec279c342ea36ac76fed6397e8c354c0a4bc
Akshay <akshay.patel@propvivo.com>
2025-04-12 12:58:13 +0530

    resale captcha solution

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,34 +1,42 @@
-import Document, { DocumentContext, Head, Html, Main, NextScript } from "next/document";
+import Document, {
+  DocumentContext,
+  Head,
+  Html,
+  Main,
+  NextScript,
+} from "next/document";
 // import { GA_TRACKING_ID } from "../lib/gtag";
-import 'regenerator-runtime/runtime';
+import "regenerator-runtime/runtime";
 
 export default class MyDocument extends Document {
-    static async getInitialProps(ctx: DocumentContext) {
-        return await Document.getInitialProps(ctx);
-    }
-    render(): JSX.Element {
-        return (
-            <Html>
-                <Head>
-                <title>Homeowners Association and Condo Management Company in Seattle | Community Association Management</title>
-                <link rel="shortcut icon" href="./favicon.ico" />
-                    {/* <script async src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`} /> */}
-                    <script
-                        dangerouslySetInnerHTML={{
-                            __html: `
+  static async getInitialProps(ctx: DocumentContext) {
+    return await Document.getInitialProps(ctx);
+  }
+  render(): JSX.Element {
+    return (
+      <Html>
+        <Head>
+          <title>
+            Homeowners Association and Condo Management Company in Seattle |
+            Community Association Management
+          </title>
+          <link rel="shortcut icon" href="./favicon.ico" />
+          {/* <script async src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`} /> */}
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
             window.dataLayer = window.dataLayer || [];
             function gtag(){dataLayer.push(arguments);}
             gtag('js', new Date());
           `,
-                        }}
-                    />
-                    
-                </Head>
-                <body>
-                    <Main />
-                    <NextScript />
-                </body>
-            </Html>
-        );
-    }
+            }}
+          />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
 }
